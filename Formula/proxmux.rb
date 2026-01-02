d2d52663237a99e2f020b99bc5c2f4b3c576ca01
Roshan <roshie548@gmail.com>
2026-01-02 15:01:13 -0500

    Update proxmux to 0.3.0

--- Formula/proxmux.rb
+++ Formula/proxmux.rb
@@ -1,26 +1,26 @@
 class Proxmux < Formula
   desc "Terminal UI for managing Proxmox VE clusters"
   homepage "https://github.com/roshie548/proxmux"
-  version "0.2.2"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/roshie548/proxmux/releases/download/v0.2.2/proxmux-darwin-arm64"
-      sha256 "6bdf0409a42e78cdbfc3e04f9a7ad19c60e9bed6b2e9395ee475306de12cde5b"
+      url "https://github.com/roshie548/proxmux/releases/download/v0.3.0/proxmux-darwin-arm64"
+      sha256 "63e61ad0d98708563b85e3f6976ccbf062083f2015f011a0401964e043c61676"
     else
-      url "https://github.com/roshie548/proxmux/releases/download/v0.2.2/proxmux-darwin-x64"
-      sha256 "d744ebf09840e4df03e53fdb9142e2bcebed985e86ec7c0e9cf1ecdcf7e7706c"
+      url "https://github.com/roshie548/proxmux/releases/download/v0.3.0/proxmux-darwin-x64"
+      sha256 "6923672f0a0100d18c4ae644e01afccd1a6573a5d28ad1f1ddaee92c82d761b8"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/roshie548/proxmux/releases/download/v0.2.2/proxmux-linux-arm64"
-      sha256 "d38c5a7a05956195fd1490d94e52350ba417920adfaa476bd0e6f539892e5fd1"
+      url "https://github.com/roshie548/proxmux/releases/download/v0.3.0/proxmux-linux-arm64"
+      sha256 "630ade59b24f8a21b312070bfc00ec416b67a0460278e315016ac9c7466d1df2"
     else
-      url "https://github.com/roshie548/proxmux/releases/download/v0.2.2/proxmux-linux-x64"
-      sha256 "839f873d89a9a437fad17d73b7f45ade8266e1dcbb1b66ab986bfdde5f6dd94c"
+      url "https://github.com/roshie548/proxmux/releases/download/v0.3.0/proxmux-linux-x64"
+      sha256 "78ca6ec0278de05901218b0015583fa78ac0caa77b30eb797e7b48cc7922197b"
     end
   end
 
